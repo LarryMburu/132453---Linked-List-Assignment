759e57f3176f444e5d1f843ffbdce9896a46a1e5
LarryMburu <168400774+LarryMburu@users.noreply.github.com>
2025-05-09 21:21:08 +0300

    Update linkedlist.py

--- linkedlist.py
+++ linkedlist.py
@@ -1,1 +1,8 @@
-linkedlist.py
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+so first thing first we have to create a representation for each node on the list. This is done using the data which stores the value of the data
+class LinkedList:
+
